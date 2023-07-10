bf2a5755ab43f378470fea00fa56231a360ac3bb
Titus Wormer <tituswormer@gmail.com>
2023-07-10 13:16:24 +0200

    Refactor code-style

--- test.js
+++ test.js
@@ -4,298 +4,358 @@ import {fromMarkdown} from 'mdast-util-from-markdown'
 import {toMarkdown} from 'mdast-util-to-markdown'
 import {gfmFootnote} from 'micromark-extension-gfm-footnote'
 import {gfmFootnoteFromMarkdown, gfmFootnoteToMarkdown} from './index.js'
-import * as mod from './index.js'
 
-test('core', () => {
-  assert.deepEqual(
-    Object.keys(mod).sort(),
-    ['gfmFootnoteFromMarkdown', 'gfmFootnoteToMarkdown'],
-    'should expose the public api'
-  )
+test('core', async function (t) {
+  await t.test('should expose the public api', async function () {
+    assert.deepEqual(Object.keys(await import('./index.js')).sort(), [
+      'gfmFootnoteFromMarkdown',
+      'gfmFootnoteToMarkdown'
+    ])
+  })
 })
 
-test('gfmFootnoteFromMarkdown', () => {
-  assert.deepEqual(
-    fromMarkdown('[^a]: b\nc\n\n    d', {
-      extensions: [gfmFootnote()],
-      mdastExtensions: [gfmFootnoteFromMarkdown()]
-    }),
-    {
-      type: 'root',
-      children: [
+test('gfmFootnoteFromMarkdown', async function (t) {
+  await t.test('should support a footnote definition', async function () {
+    assert.deepEqual(
+      fromMarkdown('[^a]: b\nc\n\n    d', {
+        extensions: [gfmFootnote()],
+        mdastExtensions: [gfmFootnoteFromMarkdown()]
+      }),
+      {
+        type: 'root',
+        children: [
+          {
+            type: 'footnoteDefinition',
+            identifier: 'a',
+            label: 'a',
+            children: [
+              {
+                type: 'paragraph',
+                children: [
+                  {
+                    type: 'text',
+                    value: 'b\nc',
+                    position: {
+                      start: {line: 1, column: 7, offset: 6},
+                      end: {line: 2, column: 2, offset: 9}
+                    }
+                  }
+                ],
+                position: {
+                  start: {line: 1, column: 7, offset: 6},
+                  end: {line: 2, column: 2, offset: 9}
+                }
+              },
+              {
+                type: 'paragraph',
+                children: [
+                  {
+                    type: 'text',
+                    value: 'd',
+                    position: {
+                      start: {line: 4, column: 5, offset: 15},
+                      end: {line: 4, column: 6, offset: 16}
+                    }
+                  }
+                ],
+                position: {
+                  start: {line: 4, column: 5, offset: 15},
+                  end: {line: 4, column: 6, offset: 16}
+                }
+              }
+            ],
+            position: {
+              start: {line: 1, column: 1, offset: 0},
+              end: {line: 4, column: 6, offset: 16}
+            }
+          }
+        ],
+        position: {
+          start: {line: 1, column: 1, offset: 0},
+          end: {line: 4, column: 6, offset: 16}
+        }
+      }
+    )
+  })
+
+  await t.test('should support a footnote call', async function () {
+    assert.deepEqual(
+      fromMarkdown('Call.[^a]\n[^a]: b', {
+        extensions: [gfmFootnote()],
+        mdastExtensions: [gfmFootnoteFromMarkdown()]
+      }).children[0],
+      {
+        type: 'paragraph',
+        children: [
+          {
+            type: 'text',
+            value: 'Call.',
+            position: {
+              start: {line: 1, column: 1, offset: 0},
+              end: {line: 1, column: 6, offset: 5}
+            }
+          },
+          {
+            type: 'footnoteReference',
+            identifier: 'a',
+            label: 'a',
+            position: {
+              start: {line: 1, column: 6, offset: 5},
+              end: {line: 1, column: 10, offset: 9}
+            }
+          }
+        ],
+        position: {
+          start: {line: 1, column: 1, offset: 0},
+          end: {line: 1, column: 10, offset: 9}
+        }
+      }
+    )
+  })
+
+  await t.test(
+    'should support a footnote after an exclamation mark',
+    async function () {
+      assert.deepEqual(
+        fromMarkdown('![^a]\n[^a]: b', {
+          extensions: [gfmFootnote()],
+          mdastExtensions: [gfmFootnoteFromMarkdown()]
+        }).children[0],
         {
-          type: 'footnoteDefinition',
-          identifier: 'a',
-          label: 'a',
+          type: 'paragraph',
           children: [
             {
-              type: 'paragraph',
-              children: [
-                {
-                  type: 'text',
-                  value: 'b\nc',
-                  position: {
-                    start: {line: 1, column: 7, offset: 6},
-                    end: {line: 2, column: 2, offset: 9}
-                  }
-                }
-              ],
+              type: 'text',
+              value: '!',
               position: {
-                start: {line: 1, column: 7, offset: 6},
-                end: {line: 2, column: 2, offset: 9}
+                start: {line: 1, column: 1, offset: 0},
+                end: {line: 1, column: 2, offset: 1}
               }
             },
             {
-              type: 'paragraph',
-              children: [
-                {
-                  type: 'text',
-                  value: 'd',
-                  position: {
-                    start: {line: 4, column: 5, offset: 15},
-                    end: {line: 4, column: 6, offset: 16}
-                  }
-                }
-              ],
+              type: 'footnoteReference',
+              identifier: 'a',
+              label: 'a',
               position: {
-                start: {line: 4, column: 5, offset: 15},
-                end: {line: 4, column: 6, offset: 16}
+                start: {line: 1, column: 2, offset: 1},
+                end: {line: 1, column: 6, offset: 5}
               }
             }
           ],
           position: {
             start: {line: 1, column: 1, offset: 0},
-            end: {line: 4, column: 6, offset: 16}
-          }
-        }
-      ],
-      position: {
-        start: {line: 1, column: 1, offset: 0},
-        end: {line: 4, column: 6, offset: 16}
-      }
-    },
-    'should support a footnote definition'
-  )
-
-  assert.deepEqual(
-    fromMarkdown('Call.[^a]\n[^a]: b', {
-      extensions: [gfmFootnote()],
-      mdastExtensions: [gfmFootnoteFromMarkdown()]
-    }).children[0],
-    {
-      type: 'paragraph',
-      children: [
-        {
-          type: 'text',
-          value: 'Call.',
-          position: {
-            start: {line: 1, column: 1, offset: 0},
-            end: {line: 1, column: 6, offset: 5}
-          }
-        },
-        {
-          type: 'footnoteReference',
-          identifier: 'a',
-          label: 'a',
-          position: {
-            start: {line: 1, column: 6, offset: 5},
-            end: {line: 1, column: 10, offset: 9}
-          }
-        }
-      ],
-      position: {
-        start: {line: 1, column: 1, offset: 0},
-        end: {line: 1, column: 10, offset: 9}
-      }
-    },
-    'should support a footnote call'
-  )
-
-  assert.deepEqual(
-    fromMarkdown('![^a]\n[^a]: b', {
-      extensions: [gfmFootnote()],
-      mdastExtensions: [gfmFootnoteFromMarkdown()]
-    }).children[0],
-    {
-      type: 'paragraph',
-      children: [
-        {
-          type: 'text',
-          value: '!',
-          position: {
-            start: {line: 1, column: 1, offset: 0},
-            end: {line: 1, column: 2, offset: 1}
-          }
-        },
-        {
-          type: 'footnoteReference',
-          identifier: 'a',
-          label: 'a',
-          position: {
-            start: {line: 1, column: 2, offset: 1},
             end: {line: 1, column: 6, offset: 5}
           }
         }
-      ],
-      position: {
-        start: {line: 1, column: 1, offset: 0},
-        end: {line: 1, column: 6, offset: 5}
-      }
-    },
-    'should support a footnote after an exclamation mark'
+      )
+    }
   )
 })
 
-test('gfmFootnoteToMarkdown', () => {
-  assert.deepEqual(
-    toMarkdown(
-      {type: 'footnoteReference', identifier: 'a'},
-      {extensions: [gfmFootnoteToMarkdown()]}
-    ),
-    '[^a]\n',
-    'should serialize a footnote reference w/ identifier'
+test('gfmFootnoteToMarkdown', async function (t) {
+  await t.test(
+    'should serialize a footnote reference w/ identifier',
+    async function () {
+      assert.deepEqual(
+        toMarkdown(
+          {type: 'footnoteReference', identifier: 'a'},
+          {extensions: [gfmFootnoteToMarkdown()]}
+        ),
+        '[^a]\n'
+      )
+    }
   )
 
-  assert.deepEqual(
-    toMarkdown(
-      // @ts-expect-error: `identifier` missing.
-      {type: 'footnoteReference', label: 'X]Y'},
-      {extensions: [gfmFootnoteToMarkdown()]}
-    ),
-    '[^X\\]Y]\n',
-    'should serialize a footnote reference w/ label'
+  await t.test(
+    'should serialize a footnote reference w/ label',
+    async function () {
+      assert.deepEqual(
+        toMarkdown(
+          // @ts-expect-error: `identifier` missing.
+          {type: 'footnoteReference', label: 'X]Y'},
+          {extensions: [gfmFootnoteToMarkdown()]}
+        ),
+        '[^X\\]Y]\n'
+      )
+    }
   )
 
-  assert.deepEqual(
-    toMarkdown(
-      {
-        type: 'paragraph',
-        children: [
-          {type: 'text', value: 'a'},
-          {type: 'footnoteReference', label: 'b', identifier: 'b'},
-          {type: 'text', value: 'c'}
-        ]
-      },
-      {extensions: [gfmFootnoteToMarkdown()]}
-    ),
-    'a[^b]c\n',
-    'should serialize a footnote reference in a paragraph'
+  await t.test(
+    'should serialize a footnote reference in a paragraph',
+    async function () {
+      assert.deepEqual(
+        toMarkdown(
+          {
+            type: 'paragraph',
+            children: [
+              {type: 'text', value: 'a'},
+              {type: 'footnoteReference', label: 'b', identifier: 'b'},
+              {type: 'text', value: 'c'}
+            ]
+          },
+          {extensions: [gfmFootnoteToMarkdown()]}
+        ),
+        'a[^b]c\n'
+      )
+    }
   )
 
-  assert.deepEqual(
-    toMarkdown(
-      // @ts-expect-error: `children` missing.
-      {type: 'footnoteDefinition', identifier: 'a'},
-      {extensions: [gfmFootnoteToMarkdown()]}
-    ),
-    '[^a]:\n',
-    'should serialize a footnote definition w/ identifier'
+  await t.test(
+    'should serialize a footnote definition w/ identifier',
+    async function () {
+      assert.deepEqual(
+        toMarkdown(
+          // @ts-expect-error: `children` missing.
+          {type: 'footnoteDefinition', identifier: 'a'},
+          {extensions: [gfmFootnoteToMarkdown()]}
+        ),
+        '[^a]:\n'
+      )
+    }
   )
 
-  assert.deepEqual(
-    toMarkdown(
-      // @ts-expect-error: `identifier` missing.
-      {type: 'footnoteDefinition', label: 'X]Y'},
-      {extensions: [gfmFootnoteToMarkdown()]}
-    ),
-    '[^X\\]Y]:\n',
-    'should serialize a footnote definition w/ label'
+  await t.test(
+    'should serialize a footnote definition w/ label',
+    async function () {
+      assert.deepEqual(
+        toMarkdown(
+          // @ts-expect-error: `identifier` missing.
+          {type: 'footnoteDefinition', label: 'X]Y'},
+          {extensions: [gfmFootnoteToMarkdown()]}
+        ),
+        '[^X\\]Y]:\n'
+      )
+    }
   )
 
-  assert.deepEqual(
-    toMarkdown(
-      {
-        type: 'footnoteDefinition',
-        label: 'a',
-        identifier: 'a',
-        children: [
-          {type: 'paragraph', children: [{type: 'text', value: 'b\nc'}]},
-          {type: 'paragraph', children: [{type: 'text', value: 'd'}]}
-        ]
-      },
-      {extensions: [gfmFootnoteToMarkdown()]}
-    ),
-    '[^a]: b\n    c\n\n    d\n',
-    'should serialize a footnote definition w/ content'
+  await t.test(
+    'should serialize a footnote definition w/ content',
+    async function () {
+      assert.deepEqual(
+        toMarkdown(
+          {
+            type: 'footnoteDefinition',
+            label: 'a',
+            identifier: 'a',
+            children: [
+              {type: 'paragraph', children: [{type: 'text', value: 'b\nc'}]},
+              {type: 'paragraph', children: [{type: 'text', value: 'd'}]}
+            ]
+          },
+          {extensions: [gfmFootnoteToMarkdown()]}
+        ),
+        '[^a]: b\n    c\n\n    d\n'
+      )
+    }
   )
 
-  assert.deepEqual(
-    toMarkdown(
-      {
-        type: 'footnoteDefinition',
-        label: 'a',
-        identifier: 'a',
-        children: [{type: 'code', value: 'b'}]
-      },
-      {extensions: [gfmFootnoteToMarkdown()]}
-    ),
-    '[^a]: ```\n    b\n    ```\n',
-    'should serialize code in a footnote definition'
+  await t.test(
+    'should serialize code in a footnote definition',
+    async function () {
+      assert.deepEqual(
+        toMarkdown(
+          {
+            type: 'footnoteDefinition',
+            label: 'a',
+            identifier: 'a',
+            children: [{type: 'code', value: 'b'}]
+          },
+          {extensions: [gfmFootnoteToMarkdown()]}
+        ),
+        '[^a]: ```\n    b\n    ```\n'
+      )
+    }
   )
 
-  assert.deepEqual(
-    toMarkdown(
-      {
-        type: 'footnoteDefinition',
-        label: 'a',
-        identifier: 'a',
-        children: [
-          {type: 'paragraph', children: [{type: 'text', value: 'b'}]},
-          {type: 'code', value: 'c'}
-        ]
-      },
-      {extensions: [gfmFootnoteToMarkdown()]}
-    ),
-    '[^a]: b\n\n    ```\n    c\n    ```\n',
-    'should serialize code as the 2nd child in a footnote definition'
+  await t.test(
+    'should serialize code as the 2nd child in a footnote definition',
+    async function () {
+      assert.deepEqual(
+        toMarkdown(
+          {
+            type: 'footnoteDefinition',
+            label: 'a',
+            identifier: 'a',
+            children: [
+              {type: 'paragraph', children: [{type: 'text', value: 'b'}]},
+              {type: 'code', value: 'c'}
+            ]
+          },
+          {extensions: [gfmFootnoteToMarkdown()]}
+        ),
+        '[^a]: b\n\n    ```\n    c\n    ```\n'
+      )
+    }
   )
 
-  assert.deepEqual(
-    toMarkdown(
-      {type: 'paragraph', children: [{type: 'text', value: 'b^[a]'}]},
-      {extensions: [gfmFootnoteToMarkdown()]}
-    ),
-    'b^\\[a]\n',
-    'should escape what would otherwise be an inline note'
+  await t.test(
+    'should escape what would otherwise be an inline note',
+    async function () {
+      assert.deepEqual(
+        toMarkdown(
+          {type: 'paragraph', children: [{type: 'text', value: 'b^[a]'}]},
+          {extensions: [gfmFootnoteToMarkdown()]}
+        ),
+        'b^\\[a]\n'
+      )
+    }
   )
 
-  assert.deepEqual(
-    toMarkdown(
-      {type: 'paragraph', children: [{type: 'text', value: 'b[^a]'}]},
-      {extensions: [gfmFootnoteToMarkdown()]}
-    ),
-    'b\\[^a]\n',
-    'should escape what would otherwise be an footnote call'
+  await t.test(
+    'should escape what would otherwise be an footnote call',
+    async function () {
+      assert.deepEqual(
+        toMarkdown(
+          {type: 'paragraph', children: [{type: 'text', value: 'b[^a]'}]},
+          {extensions: [gfmFootnoteToMarkdown()]}
+        ),
+        'b\\[^a]\n'
+      )
+    }
   )
 
-  assert.deepEqual(
-    toMarkdown(
-      {type: 'paragraph', children: [{type: 'text', value: '[a]: b'}]},
-      {extensions: [gfmFootnoteToMarkdown()]}
-    ),
-    '\\[a]: b\n',
-    'should escape what would otherwise be an footnote definition'
+  await t.test(
+    'should escape what would otherwise be an footnote definition',
+    async function () {
+      assert.deepEqual(
+        toMarkdown(
+          {type: 'paragraph', children: [{type: 'text', value: '[a]: b'}]},
+          {extensions: [gfmFootnoteToMarkdown()]}
+        ),
+        '\\[a]: b\n'
+      )
+    }
   )
 
-  assert.deepEqual(
-    toMarkdown(
-      {type: 'footnoteDefinition', identifier: 'a:b', children: []},
-      {extensions: [gfmFootnoteToMarkdown()]}
-    ),
-    '[^a:b]:\n',
-    'should support colons in footnote definitions'
+  await t.test(
+    'should support colons in footnote definitions',
+    async function () {
+      assert.deepEqual(
+        toMarkdown(
+          {type: 'footnoteDefinition', identifier: 'a:b', children: []},
+          {extensions: [gfmFootnoteToMarkdown()]}
+        ),
+        '[^a:b]:\n'
+      )
+    }
   )
 
-  assert.deepEqual(
-    toMarkdown(
-      {
-        type: 'footnoteDefinition',
-        identifier: 'a',
-        children: [{type: 'list', children: [{type: 'listItem', children: []}]}]
-      },
-      {extensions: [gfmFootnoteToMarkdown()]}
-    ),
-    '[^a]: *\n',
-    'should support lists in footnote definitions'
+  await t.test(
+    'should support lists in footnote definitions',
+    async function () {
+      assert.deepEqual(
+        toMarkdown(
+          {
+            type: 'footnoteDefinition',
+            identifier: 'a',
+            children: [
+              {type: 'list', children: [{type: 'listItem', children: []}]}
+            ]
+          },
+          {extensions: [gfmFootnoteToMarkdown()]}
+        ),
+        '[^a]: *\n'
+      )
+    }
   )
 })
