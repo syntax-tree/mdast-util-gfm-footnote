29748a55b91976608dde1ee77b21aa87c3c476ee
Titus Wormer <tituswormer@gmail.com>
2025-02-10 11:48:31 +0100

    Refactor code-style

--- test.js
+++ test.js
@@ -1,12 +1,12 @@
 import assert from 'node:assert/strict'
 import test from 'node:test'
 import {fromMarkdown} from 'mdast-util-from-markdown'
-import {toMarkdown} from 'mdast-util-to-markdown'
-import {gfmFootnote} from 'micromark-extension-gfm-footnote'
 import {
   gfmFootnoteFromMarkdown,
   gfmFootnoteToMarkdown
 } from 'mdast-util-gfm-footnote'
+import {toMarkdown} from 'mdast-util-to-markdown'
+import {gfmFootnote} from 'micromark-extension-gfm-footnote'
 
 test('core', async function (t) {
   await t.test('should expose the public api', async function () {
