cdc9c92bf3f88e2f4336aaaa21c4823bf818d2c6
Titus Wormer <tituswormer@gmail.com>
2025-02-10 11:31:14 +0100

    Refactor code-style

--- index.d.ts
+++ index.d.ts
@@ -3,16 +3,6 @@ export {gfmFootnoteFromMarkdown, gfmFootnoteToMarkdown} from './lib/index.js'
 declare module 'mdast-util-to-markdown' {
   interface ConstructNameMap {
     /**
-     * Footnote reference.
-     *
-     * ```markdown
-     * > | A[^b].
-     *      ^^^^
-     * ```
-     */
-    footnoteReference: 'footnoteReference'
-
-    /**
      * Footnote definition.
      *
      * ```markdown
@@ -21,5 +11,15 @@ declare module 'mdast-util-to-markdown' {
      * ```
      */
     footnoteDefinition: 'footnoteDefinition'
+
+    /**
+     * Footnote reference.
+     *
+     * ```markdown
+     * > | A[^b].
+     *      ^^^^
+     * ```
+     */
+    footnoteReference: 'footnoteReference'
   }
 }
